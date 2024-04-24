61413a1f1513bba6f7eabc09694034261649f003
KajalPardeshi <117445363+KajalPardeshi@users.noreply.github.com>
2024-04-24 19:45:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
     zipcode = sorted(df['statezip'].unique())
 
 
-    return render_template('ind.html', bedrooms=bedrooms, bathrooms=bathrooms, floors=floors, views=views, cities=cities, streets=streets, zipcode=zipcode)
+    return render_template('index.html', bedrooms=bedrooms, bathrooms=bathrooms, floors=floors, views=views, cities=cities, streets=streets, zipcode=zipcode)
 
 
 @app.route('/predict', methods=['POST'])
@@ -42,59 +42,7 @@ def predict():
     prediction = model.predict(input)[0]
 
     return f"Predicted Price of House is: ₹ {str(prediction)}"
-    
-
-    # prediction=model.predict(pd.DataFrame(columns=['bedrooms', 'bathrooms', 'sqft_living', 'floors', 'view', 'sqft_above', 'sqft_basement', 'street', 'city', 'statezip'], data=np.array([bedrooms,bathrooms,sqft_liv,floors,views,sqft_ab,sqft_base,cities,street,zip]).reshape(1, 10)))
-
-    # print(prediction)
-
-    # return render_template('ind.html', prediction_text='Predicted Price of House is {}'.format(prediction))
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-# Import Libraries
-# from flask import Flask, request, render_template
- 
-# import model # load model.py
- 
-# app = Flask(__name__)
- 
-# # render htmp page
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
- 
-# # get user input and the predict the output and return to user
-# @app.route('/predict',methods=['POST'])
-# def predict():
-     
-#     #take data from form and store in each feature    
-#     input_features = [x for x in request.form.values()]
-#     bath = input_features[0]
-#     balcony = input_features[1]
-#     total_sqft_int = input_features[2]
-#     bhk = input_features[3]
-#     price_per_sqft = input_features[4]
-#     area_type = input_features[5]
-#     availability = input_features[6]
-#     location = input_features[7]
-     
-#     # predict the price of house by calling model.py
-#     predicted_price = model.predict_house_price(bath,balcony,total_sqft_int,bhk,price_per_sqft,area_type,availability,location)       
- 
- 
-#     # render the html page and show the output
-#     return render_template('index.html', prediction_text='Predicted Price of Bangalore House is {}'.format(predicted_price))
- 
-# # if __name__ == "__main__":
-# #     app.run(host="0.0.0.0", port="8080")
-     
-# if __name__ == "__main__":
-#     app.run()
